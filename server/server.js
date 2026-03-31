e042dc34a988c762e6ea8fb38a6bb433b460ab92
Your Name <you@example.com>
2026-03-31 19:34:19 +0530

    mongo env fix

--- server/server.js
+++ server/server.js
@@ -11,9 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://bblkumar8_db_user:BBlk1973MMkrSH@cluster0.baicjxm.mongodb.net/?retryWrites=true&w=majority")
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
 const videoSchema = new mongoose.Schema({
   url: String,
