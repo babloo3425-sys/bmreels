0a5dcfc76f2611843dc24b5450d54f71eb54b1a8
Your Name <you@example.com>
2026-03-31 22:02:13 +0530

    fix upload limit

--- server/server.js
+++ server/server.js
@@ -10,6 +10,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
 require("dotenv").config();
 
@@ -113,7 +115,10 @@ const storage = multer.diskStorage({
   }
 });
 
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+  limits: { fileSize: 200 * 1024 * 1024 } // 200MB
+});
 
 // get likes
 app.get("/api/likes", (req, res) => {
